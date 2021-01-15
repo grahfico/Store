55d6b91a13d3f5c4b7e392eee28ae8eb94463d61
Bruno Campos <grahfico@gmail.com>
2021-01-15 22:34:32 +0000

    Add edit Image

---
+++ Store.Web/Models/ProductViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Store.Web.Models
+{
+    using Microsoft.AspNetCore.Http;
+    using Store.Web.Data.Entities;
+    using System.ComponentModel.DataAnnotations;
+
+    public class ProductViewModel : Product
+    {
+        [Display(Name = "Image")]
+        public IFormFile ImageFile { get; set; }
+
+    }
+}
